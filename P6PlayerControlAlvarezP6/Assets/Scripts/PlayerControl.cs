38d216250ea6a6ebff8bed469eead9711fbe1a4a
741076-ship-it <741076@student.dusd.net>
2026-02-04 13:50:09 -0800

    finished main
    just finished piramid
    
    currently working on oncoming traffic

--- P6PlayerControlAlvarezP6/Assets/Scripts/PlayerControl.cs
+++ P6PlayerControlAlvarezP6/Assets/Scripts/PlayerControl.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    public float speed = 5.0f;
+    private float speed = 20.0f;
+    private float turnspeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,7 +16,12 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Moves the car forward based on verticle imput
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnspeed * horizontalInput * Time.deltaTime);
+        
     }
 }
